c9d80c5ed09458a8e5cd9f54c1c4066d2ed5e88f
AseemGrover-Developer <aseemgrover1908@gmail.com>
2025-06-15 13:24:08 +0530

    Delete quiz2 directory

--- quiz2/navbar.js
+++
@@ -1,8 +0,0 @@
-// navbar.js
-
-const hamburger = document.getElementById("hamburger");
-const navbarLinks = document.getElementById("navbarLinks");
-
-hamburger.addEventListener("click", () => {
-  navbarLinks.classList.toggle("show");
-});
